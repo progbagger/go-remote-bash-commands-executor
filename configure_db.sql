dff6def0d61ef22996cc01bd2428334f277d816d
progbagger <thrixstudio@yandex.ru>
2024-05-13 02:14:51 +0700

    feat: Added database configuration script

---
+++ configure_db.sql
@@ -0,0 +1,45 @@
+CREATE TABLE IF NOT EXISTS commands (
+    id SERIAL PRIMARY KEY,
+    command TEXT NOT NULL,
+    args TEXT ARRAY
+);
+
+CREATE TABLE IF NOT EXISTS inputs (
+    id SERIAL REFERENCES commands (id),
+    input TEXT
+);
+
+CREATE TABLE IF NOT EXISTS outputs (
+    id SERIAL REFERENCES commands (id),
+    output TEXT,
+    errors TEXT
+);
+
+CREATE TABLE IF NOT EXISTS statuses (
+    id SERIAL REFERENCES commands (id),
+    exit_code INTEGER,
+    status TEXT NOT NULL
+);
+
+CREATE OR REPLACE FUNCTION outputs_statuses_trigger_fnc()
+RETURNS trigger AS
+$$
+BEGIN
+INSERT INTO outputs (id) VALUES (
+  NEW.id
+);
+INSERT INTO statuses (id, status) VALUES (
+  NEW.id,
+  'RUNNING'
+);
+RETURN NEW;
+END;
+$$ LANGUAGE "plpgsql";
+
+CREATE
+OR
+REPLACE
+    TRIGGER after_command_insert AFTER
+INSERT
+    ON commands FOR EACH ROW
+EXECUTE PROCEDURE outputs_statuses_trigger_fnc ();
